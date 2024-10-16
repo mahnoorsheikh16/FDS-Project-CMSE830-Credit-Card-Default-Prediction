ee26ccd23fe9c2976a26d0b52051bd240fa3628c
Mahnoor Sheikh <67545978+mahnoorsheikh16@users.noreply.github.com>
2024-10-15 20:13:42 -0400

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -48,7 +48,7 @@ if page == "🏡Home":
     with col3:
         st.metric("**Default Percentage**", f"{perc:,}%")
     with col4:
-        st.metric("**Total NTD Lost**", f"${money_lost:,}", delta="⬇", delta_color="inverse")
+        st.metric("**Total NTD Lost**", f"${money_lost:,}")
 
 elif page == "👪Demographic Data":
     st.subheader("Defaults in relation to Gender, Relationship Status, Age, & Education Level")
